301e1cdeec550738694bb4b5a62bed49a43a6f89
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-02-08 14:34:03 -0800

    Add starter file for documentation challenge

---
+++ week-5/method_detective.rb
@@ -0,0 +1,33 @@
+# “iNvEsTiGaTiOn”.<???>
+# => “InVeStIgAtIoN”
+
+# “zom”.<???>
+# => “zoom”
+
+# “enhance”.<???>
+#=> “    enhance    ”
+
+# “Stop! You’re under arrest!”.<???>
+# => “STOP! YOU’RE UNDER ARREST!”
+
+# “the usual”.<???>
+#=> “the usual suspects”
+
+#“ suspects”.<???>
+#=> “the usual suspects”
+
+# “The case of the disappearing last letter”.<???>
+# => “The case of the disappearing last lette”
+
+# “The mystery of the missing first letter”.<???>
+# => “he mystery of the missing first letter”
+
+# “Elementary  ,    my   dear      Watson!”.<???>
+# => “Elementary, my dear Watson!”
+
+#“z”.<???>
+# => 122 
+# (What is the significance of the number 122 in relation to the character z?)
+
+#“How many times does the letter ‘a’ appear in this string?”.<???>
+#=> 4
