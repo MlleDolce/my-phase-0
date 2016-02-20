d19ec9630326b71bcebebe83348747b38f9af76a
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-02-19 19:11:33 -0800

    Add tests for RSpec challenge

---
+++ week-6/list/todo_list_spec.rb
@@ -0,0 +1,21 @@
+describe TodoList do
+  let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
+
+  it "initializes with two items" do
+    expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
+  end
+
+  it "adds an item to the list" do
+    list.add_item("mop")
+    expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
+  end
+
+  it "deletes an item" do
+    list.delete_item("do the dishes")
+    expect(list.get_items).to eq ["mow the lawn"]
+  end
+
+  it "retrieves an item by index" do
+    expect(list.get_item(0)).to eq "do the dishes"
+  end
+end
