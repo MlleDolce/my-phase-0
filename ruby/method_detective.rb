a2f6c7f824f17f85ba3a6eb70fa2e311df890c6f
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-03-28 13:20:13 -0700

    Fix funky quotes

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,36 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
+# "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
 
-# “zom”.<???>
+# "zom".<???>
 # => “zoom”
 
-# “enhance”.<???>
-#=> “    enhance    ”
+# "enhance".<???>
+#=> "    enhance    "
 
-# “Stop! You’re under arrest!”.<???>
-# => “STOP! YOU’RE UNDER ARREST!”
+# "Stop! You’re under arrest!".<???>
+# => "STOP! YOU’RE UNDER ARREST!"
 
-# “the usual”.<???>
-#=> “the usual suspects”
+# "the usual".<???>
+#=> "the usual suspects"
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+# " suspects".<???>
+#=> "the usual suspects"
 
-# “The case of the disappearing last letter”.<???>
-# => “The case of the disappearing last lette”
+# "The case of the disappearing last letter".<???>
+# => "The case of the disappearing last lette"
 
-# “The mystery of the missing first letter”.<???>
-# => “he mystery of the missing first letter”
+# "The mystery of the missing first letter".<???>
+# => "he mystery of the missing first letter"
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
-# => “Elementary, my dear Watson!”
+# "Elementary  ,    my   dear      Watson!".<???>
+# => "Elementary, my dear Watson!"
 
-#“z”.<???>
+# "z".<???>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+# "How many times does the letter 'a' appear in this string?".<???>
 #=> 4
