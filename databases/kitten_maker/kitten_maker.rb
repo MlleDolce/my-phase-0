4e566127e1b069149cb8959a79315f09ddfa03d1
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-04-15 11:52:55 -0700

    Add program that uses persistent data

---
+++ databases/kitten_maker/kitten_maker.rb
@@ -0,0 +1,44 @@
+# OPERATION KITTEN EXPLOSION!
+
+# talk about Object Relational Mapping (ORM)
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database
+db = SQLite3::Database.new("kittens.db")
+db.results_as_hash = true
+
+# learn about fancy string delimiters
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS kittens(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+  )
+SQL
+
+# create a kittens table (if it's not there already)
+db.execute(create_table_cmd)
+
+# add a test kitten
+# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+
+# add LOOOOTS of kittens!
+# so. many. kittens. 
+#KittenExplosion
+def create_kitten(db, name, age)
+  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+end
+
+10000.times do
+  create_kitten(db, Faker::Name.name, 0)
+end
+
+# explore ORM by retrieving data
+# kittens = db.execute("SELECT * FROM kittens")
+# kittens.each do |kitten|
+#  puts "#{kitten['name']} is #{kitten['age']}"
+# end
+
