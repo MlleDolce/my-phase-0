7db44deedd8ab7c81c4ec9abd7ae671f079cc841
MlleDolce <andreakchristie@gmail.com>
2016-07-01 23:12:20 -0400

    Added vampire quiz data

---
+++ ruby/vampires.rb
@@ -0,0 +1,9 @@
+#Vampire-detection quiz
+
+puts "What is your name?"
+user_name = gets.chomp.to_s
+puts "How old are you?  What year were you born?"
+age = gets.chomp.to_i
+puts "Our company serves garlic bread.  Should we order some for you? (y/n)"
+garlic_desire = gets.chomp.to_s
+puts "Would you like to enroll in the company's health insurance?"
