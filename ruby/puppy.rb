96e72eab6bebc0274b5ec73c602b4c612e63881d
MlleDolce <andreakchristie@gmail.com>
2016-07-11 19:18:25 -0400

    Created puppy class and wrote pseudo-code for characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+Species ----------------
+Puppies (Canis lupus familiaris)
+
+Characteristics -------
+size: small
+friendly: yes
+legs: 4
+furry: yes
+
+Behavior ---------------
+Play
+Lick
+Sleep
