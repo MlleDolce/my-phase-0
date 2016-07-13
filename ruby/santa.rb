365497e4af5a53ef244d3c7ed4e68749b0832a43
MlleDolce <andreakchristie@gmail.com>
2016-07-13 11:16:34 -0400

    Added celebrate_birthday, get_mad_at, and getter methods for @age and @ethnicity

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,14 @@
+santas = []
+
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing new Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", 
+		"Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -9,11 +16,57 @@ class Santa
 	end
 
 	def eat_milk_and_cookies(cookie_type)
-		puts "That was a good #{cookie_type}"
+		puts "That was a good #{cookie_type} cookies"
+	end
+
+	def about
+		puts "Age: #{@age}"
+		puts "Gender: #{@gender}"
+		puts "Ethnicity: #{@ethnicity}"
+	end
+
+	def celebrate_birthday(num_of_birthdays)
+		@age += num_of_birthdays
+	end
+
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking.push(reindeer_name)
+		puts @reindeer_ranking
+	end
+
+	def reassign_gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
 	end
 end
 
-Andrea = Santa.new
+genders = ["male", "female", "bi-gender", "N/A", "male-female", "female-male"]
+ethnicities = ["white", "korean", "brasilian", "chinese", "black", "hispanic"]
 
-Andrea.speak
-Andrea.eat_milk_and_cookies("chocolate chip")
+genders.length.times do |index|
+	santas << Santa.new(genders[index], ethnicities[index])
+end
+
+puts "Testing each Santa instance to learn about each santa..."
+santas.each do |santa|
+	santa.celebrate_birthday(1)
+	santa.about
+end
+
+Andrea = Santa.new("female", "white")
+
+p Andrea.get_mad_at("Rudolph")
+
+p Andrea.reassign_gender = "unicorn"
+p Andrea.age
+p Andrea.ethnicity
+
+
