e97d72e2a203fcc2253b7e883d2a08adf4570d9e
MlleDolce <andreakchristie@gmail.com>
2016-07-13 12:05:11 -0400

    Added santa creator driver code and santa age generator method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -36,27 +36,55 @@ class Santa
 		@reindeer_ranking.push(reindeer_name)
 		puts @reindeer_ranking
 	end
+
+	def age_generator
+		ages = []
+		year = 0
+		until ages.length == 141
+			ages << year
+			year += 1
+		end
+		p ages.sample
+	end
 end
 
-genders = ["male", "female", "bi-gender", "N/A", "male-female", "female-male"]
-ethnicities = ["white", "korean", "brasilian", "chinese", "black", "hispanic"]
+genders = ["male", "female", "bi-gender", "N/A", "male-female", "female-male", "genderless"]
+ethnicities = ["white", "korean", "brasilian", "chinese", "black", 
+	"hispanic", "first nations", "japanese"]
 
-genders.length.times do |index|
-	santas << Santa.new(genders[index], ethnicities[index])
-end
+#genders.length.times do |index|
+#	santas << Santa.new(genders[index], ethnicities[index])
+#end
 
+=begin
 puts "Testing each Santa instance to learn about each santa..."
 santas.each do |santa|
 	santa.celebrate_birthday(1)
 	santa.about
 end
+=end
 
-Andrea = Santa.new("female", "white")
+#Release 3
+#Andrea = Santa.new("female", "white")
 
-p Andrea.get_mad_at("Rudolph")
+#p Andrea.get_mad_at("Rudolph")
 
-p Andrea.gender = "unicorn"
-p Andrea.age = 30
-p Andrea.ethnicity
+#p Andrea.gender = "unicorn"
+#p Andrea.age = 30
+#p Andrea.ethnicity
+
+#Release 4
+
+#age_loop
+
+until santas.length == 50
+	santas << Santa.new(genders.sample, ethnicities.sample)
+end
+
+santas.each do |santa| 
+	santa.age = santa.age_generator
+	p santa.gender
+	p santa.ethnicity
+end
 
 
