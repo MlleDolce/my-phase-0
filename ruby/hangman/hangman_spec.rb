1ef69040fa3cf2de3baf1ae2cb50ccb34ad6ab5e
MlleDolce <andreakchristie@gmail.com>
2016-08-08 17:10:39 -0400

    Created run_game and user_interface methods for Hangman Class.  Refactored pseudocode.  Created three spec tests that pass.

--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -1,9 +1,17 @@
 require_relative 'hangman'
 
 describe Hangman do
-	let(:game) { Hangman.new("hippopotomus") }
+	let(:game) { Hangman.new("hippopotomus", ["h","p","o","i","t","m","u","s"]) }
 
-	it "establishes an array on initialization that contains the placeholder letters for hangman" do
-		expect(game.letter_slots).to eq ["_","_","_","_","_","_","_","_","_","_","_","_"]
+	it "returns a hangman word rack to guess from" do
+		expect(game.word_rack).to eq "_ _ _ _ _ _ _ _ _ _ _ _"
+	end
+
+	it "determines if the game is over" do
+		expect(game.word_complete).to eq false
+	end	
+
+	it "returns the new number of each round that increases by one each time" do
+		expect(game.run_game("h")).to eq 2
 	end
 end
