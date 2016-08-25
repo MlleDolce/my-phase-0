402f16fc8bd2711200959ed7be54e9dc1f9861b4
MlleDolce <andreakchristie@gmail.com>
2016-08-25 19:28:27 -0400

    9.5, made 2 new erb templates: course_options and course_collection and added corresponding http routes to app.rb.  Built a form for the course_collection erb.

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,3 +1,4 @@
+
 # require gems
 require 'sinatra'
 require 'sqlite3'
@@ -5,7 +6,9 @@ require 'sqlite3'
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
+db1 = SQLite3::Database.new("courses.db")
 db.results_as_hash = true
+db1.results_as_hash = true
 
 # show students on the home page
 get '/' do
@@ -24,4 +27,19 @@ post '/students' do
   redirect '/'
 end
 
-# add static resources
+# Release 1: Create a route for your new erb page
+get '/students/courses' do
+  erb :course_options
+end
+
+# Release 2: Build a Form.  Allow the user to add or manipulate data in some way using a form
+post '/students/courses' do
+	db1.execute("INSERT INTO course_suggestions (title) VALUES (?)", [params['title1']])
+	db1.execute("INSERT INTO course_suggestions (title) VALUES (?)", [params['title2']])
+	redirect '/students/courses/collection'
+end
+
+get '/students/courses/collection' do
+	@courses = db1.execute("SELECT * FROM course_suggestions")
+	erb :course_collection
+end
